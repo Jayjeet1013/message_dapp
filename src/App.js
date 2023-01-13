188b811e1e9401f9a33f70c193ad349aec695d22
Jayjeet1013 <jayjeet1013@gmail.com>
2023-01-14 01:32:58 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,55 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
+import {ethers } from 'ethers'
+import abi from './artifacts/contracts/Message.sol/Message.json'
 import './App.css';
 
 function App() {
-const [message,setMessage]=useState();
+
+const [state,setState] = useState({
+  provider:null,
+  signer:null,
+  contract:null,
+});
+const [account,setAccount]=useState("None")
+useEffect(() => {
+   const connnectWallet= async () => {
+    const contractAddress ="0x5FbDB2315678afecb367f032d93F642f64180aa3";
+    const contractABI =abi.abi;
+    try {
+      const {ethereum} = window;
+
+      if(ethereum) {
+        const account = await ethereum.request({
+          method:"eth_requestAccounts",
+        });
+      
+       window.ethereum.on("chainChanged",()=>{
+        window.location.reload();
+       })
+
+       window.ethereum.on("accountsChanged",()=>{
+        window.location.reload();
+       })
+
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const contract =new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+      setAccount(account)
+      setState({provider,signer,contract});
+      }else{
+        alert("please intall Metamask");
+      }
+     } catch(error) {
+      console.log(error);
+     }
+
+   };
+   connnectWallet();
+},[])
 
   return (
     <div className="App">
@@ -11,8 +58,11 @@ const [message,setMessage]=useState();
          
       </header>
       <body className='body'>
-        <button type='button' className='btn'>Connect</button>
-
+      <form>
+       <input type="text"/>
+       <button type='button' className='btn'>Send</button>
+       <p>connnected wallet {account}</p>
+       </form>
       </body>
       <footer className=' footer'>
         Made by Jay
